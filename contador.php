dc94a71619ee819719afac4e7126e7dd74938f3f
Sandra <92933903+Nehins@users.noreply.github.com>
2023-01-09 11:14:19 +0100

    Creació dels comptadors

--- contador.php
+++ contador.php
@@ -1,1 +1,13 @@
-echo "hola";
+<?php
+    $count=$_COOKIE['count'];
+    if(isset($count)){
+        $count++;
+    }else{
+        $count=0;
+    }
+
+    setcookies('count',$count);
+    echo $count;
+
+?>
+
